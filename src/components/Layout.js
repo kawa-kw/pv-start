7216ca8670df20d41176839ab2d67fc23eaac5f4
kawa <ally.kaw@gmail.com>
2019-09-25 00:29:00 +0200

    rebase

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,7 +38,7 @@ const TemplateWrapper = ({ children }) => {
           href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
           color="#ff4400"
         />
-        <meta name="theme-color" content="#fff" />
+    <meta name="theme-color" content="#ff00ff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
